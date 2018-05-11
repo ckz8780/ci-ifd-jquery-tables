b86d0c16bdc3527f6656dcefaf9f5cfbaf3efc06
Chris Zielinski <ckz8780@gmail.com>
2018-05-11 01:07:14 +0000

    Completed coloring challenge

--- js/my_script.js
+++ js/my_script.js
@@ -1,4 +1,4 @@
 $(document).ready(function (){
-    $("h2").css("text-decoration", "underline"); // Underline all h2 elements
-    $("ul").css("border", "solid 1px #ccc"); // Add a border to all lists
+    $('tr:odd').css('background-color', 'orange');
+    $('tr:even').css('background-color', '#ffc6b3');
 })
