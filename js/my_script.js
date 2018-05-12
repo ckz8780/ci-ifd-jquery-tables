91bb68b864337a8091517c4895cb4fe6218f8055
Chris Zielinski <ckz8780@gmail.com>
2018-05-12 21:25:34 +0000

    added traversing the DOM challenge 2

--- js/my_script.js
+++ js/my_script.js
@@ -1,4 +1,10 @@
 $(document).ready(function (){
     $('tr:odd').css('background-color', 'orange');
     $('tr:even').css('background-color', '#ffc6b3');
-})
+    
+    // Traversing the DOM - Other Traversing Methods Challenge 2:
+    $('th').click(function() {
+        $(this).parent().children().css('background-color', 'yellow');
+        $(this).parent().siblings().children().css('background-color', 'white');
+    });
+});
